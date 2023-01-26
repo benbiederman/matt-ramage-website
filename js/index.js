a9a26a168df32f22ec2f6cf75e4e800c655c8d1f
Ben Biederman <benbiederman@gmail.com>
2023-01-26 13:02:02 -0600

    Add mobile homepage hero.

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const homepageHeroBtn = document.querySelector(".homepage-hero button");
+
+homepageHeroBtn.addEventListener("click", function () {
+  window.open("https://www.youtube.com/c/MattRamage", "_blank");
+});
