a503a478df4d5ed5c25313ba3a010411183c7000
Ben Biederman <benbiederman@gmail.com>
2023-01-30 07:13:41 -0600

    Add BSS section.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 const homepageHeroBtn = document.querySelector(".homepage-hero button");
+const bearsSuckBtn = document.querySelector(".bears-suck button");
 
 homepageHeroBtn.addEventListener("click", function () {
   window.open("https://www.youtube.com/c/MattRamage", "_blank");
+});
+
+bearsSuckBtn.addEventListener("click", function () {
+  window.open(
+    "https://www.campechecollective.com/shop/p/bears-still-suck-hoodie",
+    "_blank"
+  );
 });
