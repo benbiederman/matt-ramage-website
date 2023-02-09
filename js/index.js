4dd443697e00395a307fc52eebe4485c919d73a3
Ben Biederman <benbiederman@gmail.com>
2023-02-09 13:22:50 -0600

    Index business inquiries page.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const homepageHeroBtn = document.querySelector(".homepage-hero button");
 const bearsSuckBtn = document.querySelector(".bears-suck button");
+const businessBtn = document.querySelector(".business button");
 
 homepageHeroBtn.addEventListener("click", function () {
   window.open("https://www.youtube.com/c/MattRamage", "_blank");
@@ -10,4 +11,8 @@ bearsSuckBtn.addEventListener("click", function () {
     "https://www.campechecollective.com/shop/p/bears-still-suck-hoodie",
     "_blank"
   );
+});
+
+businessBtn.addEventListener("click", function () {
+  window.location.href = "/business-inquiries.html";
 });
