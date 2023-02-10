37cf71237e02d206a27fbffbff8ddaf6f297c398
Ben Biederman <benbiederman@gmail.com>
2023-02-10 12:43:04 -0600

    Add beer donation button.

---
+++ js/beer.js
@@ -0,0 +1,8 @@
+const beerBtn = document.querySelector(".beer-btn");
+
+beerBtn.addEventListener("click", function () {
+  window.open(
+    "https://www.paypal.com/donate/?hosted_button_id=799YH24BDUKPG",
+    "_blank"
+  );
+});
