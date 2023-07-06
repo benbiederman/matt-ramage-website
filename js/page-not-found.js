fd8750a3106837834832eac1a8c9da7bf52322da
Ben Biederman <benbiederman@gmail.com>
2023-07-06 06:36:02 -0500

    Add threads logo.

--- js/page-not-found.js
+++ js/page-not-found.js
@@ -1,5 +1,12 @@
+const cta = document.querySelector(".page-not-found-cta");
 const homepageBtn = document.querySelector(".homepage-btn");
 
 homepageBtn.addEventListener("click", () => {
   window.location.href = "/";
+});
+
+cta.addEventListener("keydown", function (e) {
+  if (e.code === "Enter") {
+    window.location.href = "/";
+  }
 });
